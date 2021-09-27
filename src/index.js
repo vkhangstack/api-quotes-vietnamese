73d7781e2977d59c2367594d6d1899bc243197ec
Pham Van Khang <thanghayphacode@gmail.com>
2021-09-28 00:02:17 +0700

    define router author

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-'use strict'
+'use strict';
 
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const connectDB = require('./database/db')
-connectDB() // connect to the database
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const connectDB = require('./database/db');
+connectDB(); // connect to the database
+const author = require('./routes/authors');
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use('/', author);
 
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`)
-})
+  console.log(`Server running on port ${PORT}...`);
+});
