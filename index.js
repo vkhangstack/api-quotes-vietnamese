ae7d820125060eeef74ed3692ac7535d1d3503e4
Pham Van Khang <thanghayphacode@gmail.com>
2021-09-27 18:11:29 +0700

    config eslint

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -6,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
