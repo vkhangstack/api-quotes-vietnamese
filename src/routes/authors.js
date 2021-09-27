76d289811a6cebf8f326c873173cbe16acce8b9a
Pham Van Khang <thanghayphacode@gmail.com>
2021-09-28 00:19:27 +0700

    handler error

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const addAuthor = require('../controller/authors/addAuthor');
 const getAuthorById = require('../controller/authors/getAuthorById');
 
-router.post('/author', async (req, res, next) => {
+router.post('/author', async (req, res) => {
   try {
     let id = req.body.id;
     let name = req.body.name;
@@ -22,22 +22,22 @@ router.post('/author', async (req, res, next) => {
       description,
       quoteCount
     );
-    if (!id || !name) return next(new Error('ID && Name not null'));
+    if (!id || !name)
+      return res.send({ status: 404, message: 'ID && Name not null' });
     res.send(author);
   } catch (error) {
-    return res.send(error);
+    return res.send({ status: 404, message: 'Bad request', error });
   }
 });
 
-router.get('/author/:id', async (req, res, next) => {
+router.get('/author/:id', async (req, res) => {
   try {
     let id = req.params.id;
     const author = await getAuthorById.getAuthorById(id);
-    if (!author) return next(new Error('User not found'));
-
+    if (!author) return res.send({ status: 404, message: 'Author not found' });
     res.send(author);
   } catch (error) {
-    return res.send(error);
+    return res.send({ status: 404, message: 'Bad request', error });
   }
 });
 
